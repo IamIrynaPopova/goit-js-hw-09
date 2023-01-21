aa1e8f0898f626c238785fa9d62b57c6b3fb7131
Iryna Popova <popovaira969@gmail.com>
2023-01-21 23:40:45 +0200

    finish first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+const { btnStart, btnStop, body } = refs;
 
+btnStart.addEventListener('click', onClickBtnStart);
+btnStop.addEventListener('click', onClickBtnStop);
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickBtnStart() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function onClickBtnStop() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerId);
+}
