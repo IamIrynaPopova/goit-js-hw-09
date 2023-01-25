148f10878c306cc2a1cd916b31cfd109f27250d7
Iryna Popova <popovaira969@gmail.com>
2023-01-25 23:54:04 +0200

    finish

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,15 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const refs = {
   input: document.getElementById('datetime-picker'),
   btnStart: document.querySelector('button[data-start]'),
   timerEL: document.querySelector('.timer'),
-  dayValue: document.querySelector('span[data-days]'),
-  hourValue: document.querySelector('span[data-hours]'),
-  minuteValue: document.querySelector('span[data-minutes]'),
-  secondValue: document.querySelector('span[data-seconds]'),
+  dayValue: document.querySelector('[data-days]'),
+  hourValue: document.querySelector('[data-hours]'),
+  minuteValue: document.querySelector('[data-minutes]'),
+  secondValue: document.querySelector('[data-seconds]'),
 };
 const {
   input,
@@ -20,18 +21,27 @@ const {
   secondValue,
 } = refs;
 
+Notiflix.Notify.init({
+  position: 'center-bottom',
+  width: '300px',
+  distance: '400px',
+});
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < new Date()) {
-      window.alert('Please choose a date in the future');
+      if (selectedDates[0] < new Date()) {
+        // window.alert('Please choose a date in the future');
+        Notiflix.Notify.failure('Please choose a date in the future');
     }
     btnStart.disabled = false;
   },
 };
+
+
 
 btnStart.disabled = true;
 
@@ -42,18 +52,25 @@ btnStart.addEventListener('click', onClickBtnStart);
 function onClickBtnStart() {
   const selectedDatе = new Date(input.value).getTime();
   const timerId = setInterval(() => {
-    const diff = selectedDatе - new Date();
+  const diff = selectedDatе - new Date();
     const convertDate = convertMs(diff);
+    console.log(convertDate);
     btnStart.disabled = true;
-    dayValue.textContent = convertDate.days;
-    hourValue.textContent = convertDate.hours;
-    minuteValue.textContent = convertDate.minutes;
-    secondValue.textContent = convertDate.seconds;
-    if (diff <= 1000) {
+    dayValue.textContent = addLeadingZero(convertDate.days);
+    hourValue.textContent = addLeadingZero(convertDate.hours);
+    minuteValue.textContent = addLeadingZero(convertDate.minutes);
+    secondValue.textContent = addLeadingZero(convertDate.seconds);
+    console.log(typeof secondValue.textContent);
+          if (diff <= 1000) {
       clearInterval(timerId);
     }
   }, 1000);
-}
+
+};
+
+function addLeadingZero(value) {
+    return value.toString().padStart(2, "0");
+};
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', getData);
+
+function getData(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.target;
+
+  for (let i= 1; i <= Number(amount.value); i += 1) {
+    let stepPromise = Number(delay.value) + Number(step.value) * (i - 1);
+   setTimeout(() => {
+     createPromise(i, stepPromise)
+       .then(({ position, delay }) => {
+         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+       })
+       .catch(({ position, delay }) => {
+         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+       });
+   }, stepPromise);
   }
+  event.currentTarget.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const newPromise = { position, delay };
+    console.log(newPromise);
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+      resolve(newPromise);
+    } else {
+      // Reject
+      reject(newPromise);
+    }
+  });
 }
